fa31e8bfa757d9befe33298116984b19a89de398
MOSES <MOSESLAZARO2019@outlook.com>
2019-10-04 07:45:31 +0300

    Updates

---
+++ precache-manifest.454085a14d160cfbf87c5b2684f2e394.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34f5c4ba75c4518e6f1248dfb91c6c5a",
+    "url": "/clock/index.html"
+  },
+  {
+    "revision": "5992675ca9a54bab9ddc",
+    "url": "/clock/static/css/main.30725471.chunk.css"
+  },
+  {
+    "revision": "69e039dd80ea760c1807",
+    "url": "/clock/static/js/2.bc74846c.chunk.js"
+  },
+  {
+    "revision": "5992675ca9a54bab9ddc",
+    "url": "/clock/static/js/main.caca8446.chunk.js"
+  },
+  {
+    "revision": "02712ce6f91aa8d9965d",
+    "url": "/clock/static/js/runtime-main.ee08f2e2.js"
+  }
+]);
